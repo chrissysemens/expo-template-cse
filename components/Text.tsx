77b29d3905a656985989a722035d6d0d91cc5ab8
chrissysemens <chrissysemens@gmail.com>
2025-12-30 16:40:18 +0100

    chore: Add first component

---
+++ components/Text.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {
+  Text as RNText,
+  TextProps as RNTextProps,
+} from 'react-native';
+
+export type TextProps = RNTextProps;
+
+export function Text(props: TextProps) {
+  return <RNText {...props} />;
+}
