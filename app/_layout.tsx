01028b9d28cf1db03789fe33e85ae674f9ac2c3f
chrissysemens <chrissysemens@gmail.com>
2025-12-31 05:07:28 +0100

    feat: add safe area

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,17 +2,20 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useTheme } from '../theme/useTheme';
 import { AppLayout } from 'layout/AppLayout';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   const { resolved, colors } = useTheme();
 
   return (
-    <AppLayout padded={true}>
-      <StatusBar
-        style={resolved === 'dark' ? 'light' : 'dark'}
-        backgroundColor={colors.background}
-      />
-      <Stack screenOptions={{ headerShown: false }} />
-    </AppLayout>
+    <SafeAreaProvider>
+      <AppLayout safe padded>
+        <StatusBar
+          style={resolved === 'dark' ? 'light' : 'dark'}
+          backgroundColor={colors.background}
+        />
+        <Stack screenOptions={{ headerShown: false }} />
+      </AppLayout>
+    </SafeAreaProvider>
   );
 }
--- layout/AppLayout.tsx
+++ layout/AppLayout.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import { View, ViewProps } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useTheme } from '../theme/useTheme';
 
 type Props = ViewProps & {
   children: React.ReactNode;
   padded?: boolean;
+  safe?: boolean;
 };
 
 export function AppLayout({
   children,
   padded = false,
+  safe = false,
   style,
   ...props
 }: Props) {
   const { colors } = useTheme();
+  const insets = useSafeAreaInsets();
 
   return (
     <View
       {...props}
       style={[
-        { flex: 1, backgroundColor: colors.background },
-        padded && { padding: 20 },
+        {
+          flex: 1,
+          backgroundColor: colors.background,
+          paddingTop: safe ? insets.top : 0,
+          paddingBottom: safe ? insets.bottom : 0,
+        },
+        padded && { paddingHorizontal: 16 },
         style,
       ]}
     >
